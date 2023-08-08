0f2131e5ce6b62ae3a9182c87b975881f9b56ab3
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-08 13:19:19 +0200

    set up pages components

---
+++ frontend/src/components/pages/home.js
@@ -0,0 +1,46 @@
+import { initElementComponent } from "../../utils/nana.js";
+
+export default () => {
+  document.body.appendChild(
+    initElementComponent({
+      type: "section",
+      id: "main",
+      children: [
+        {
+          type: "nav",
+          id: "navigation",
+          children: [
+            {
+              type: "ul",
+              id: "nav_links",
+              children: new Array(3).fill().map((_, i) => ({
+                type: "li",
+                class: "page_link",
+                // content: i === 0 ? "home" : i === 1 ? "movies" : "about",
+                children: [
+                    i === 0 ? {
+                    type: "a",
+                    content: "home",
+                    attributes: { href: "./index.html" },
+                  } :i === 1 ? {
+                    type: "a",
+                    content: "movies",
+                    attributes: { href: "./movies.html" },
+                  }:{
+                    type: "a",
+                    content: "about",
+                    attributes: { href: "./about.html" },
+                  },
+                ],
+              })),
+            },
+          ],
+        },
+        {
+          type: "h1",
+          content: "WelCome to Rusty Movies",
+        },
+      ],
+    })
+  );
+};
