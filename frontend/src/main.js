7cca33dd2b5612c6b4ac283209e829cabb41fd3d
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-08 13:16:21 +0200

    home page component works

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,3 @@
-import { apiUrl, options } from "./components/cimaTube/url.js";
-import { RequestData } from "./components/cimaTube/Request.js";
-import { home, recommended, trending } from "./components/cimaTube/api.js";
-(async () => {
-  let response;
-  response = await home(apiUrl, options);
-  console.table(response);
-  response = await recommended(apiUrl, options);
-  console.table(response);
-  response = await trending(apiUrl, options);
-  console.table(response);
-  // await request(apiUrl, RequestData);
-})();
+import home from "./components/pages/home.js";
+
+home();
