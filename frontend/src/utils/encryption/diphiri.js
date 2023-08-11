cb9314b82b4b85875a29f8c3ae353f261463926f
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-11 15:02:24 +0200

    frontend encryption and decryption in the frontend works

---
+++ frontend/src/utils/encryption/diphiri.js
@@ -0,0 +1,18 @@
+export const secret = {
+  /**
+   * @description secret to be used to generate key.
+   */
+  k: "kebalepile",
+  /**
+   * @description secrte to be used to generate intialization vector
+   */
+  iv: "motshoana",
+  /**
+   * @description Choose a fixed salt value or generate one based on your needs
+   */
+  salt: "some-salt-value",
+  /**
+   * @description Choose an appropriate number of iterations
+   */
+  iteration: 100000,
+};
