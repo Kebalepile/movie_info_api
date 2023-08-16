17ef0e8540b643d0847628be3fd3ed156802e8b9
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-16 17:19:18 +0200

    coming soon handler func & parameter

--- server/httpServer.go
+++ server/httpServer.go
@@ -176,6 +176,58 @@ func recommendedHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func comingSoonHandler(w http.ResponseWriter, r *http.Request) {
+
+	origin := r.Header.Get("Origin")
+
+	if ok := allowedDomains[origin]; !ok {
+		http.Error(w, "Forbidden Not Allowed Origin", http.StatusForbidden)
+		return
+	}
+
+	comingSoonMovies := mongo.CommingSoon()
+
+	w.Header().Set("Access-Control-Allow-Origin", origin) // Allow all origins (you can specify specific origins here)
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Content-Type", "application/json")
+
+	if len(comingSoonMovies) > 0 {
+
+		// Encrypt & Encode filesData
+		encodedText, err := encrypt.EncryptEncode(comingSoonMovies)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response, err := json.Marshal(encodedText)
+		if err != nil {
+			http.Error(w, "Failed to marshel response", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	} else {
+		custom := encrypt.CustomError{Message: "could not find recommended files"}
+		// Encrypt & Encode error message
+		encodedText, err := encrypt.EncryptEncode(custom.Error())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response, err := json.Marshal(encodedText)
+		if err != nil {
+			http.Error(w, "Failed to marshel response", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(response)
+	}
+
+}
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 	origin := r.Header.Get("Origin")
@@ -211,6 +263,7 @@ func Init() {
 	serveMux.HandleFunc("/request", requestHandler)
 	serveMux.HandleFunc("/trending", trendingHandler)
 	serveMux.HandleFunc("/recommended", recommendedHandler)
+	serveMux.HandleFunc("/coming_soon", comingSoonHandler)
 
 	log.Println("Golang API server listeing on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", serveMux))
