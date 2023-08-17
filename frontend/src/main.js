7d80a3ed2dd7c1b87a9d2d74deda7a277686b6c3
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-17 09:43:41 +0200

    movie poster slides

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,27 +7,35 @@ import {
   Recommended,
   Trending,
 } from "./components/cimaTube/api.js";
-import { apiUrl, options } from "./components/cimaTube/url.js";
+// import { apiUrl, options } from "./components/cimaTube/url.js";
 // import { Decrypt } from "./utils/encryption/encrypt.js";
-import { RequestData } from "./components/cimaTube/Request.js";
-(async () => {
-  let res = await Home(apiUrl, options);
-  console.log("Home");
-  console.table(res);
+// import { RequestData } from "./components/cimaTube/Request.js";
+// (async () => {
+//   let res = await Home(apiUrl, options);
+//   console.log("Home");
+//   console.table(res);
 
-  res = await Trending(apiUrl, options);
-  console.log("Trending");
-  console.table(res);
+//   res = await Trending(apiUrl, options);
+//   console.log("Trending");
+//   console.table(res);
 
-  res = await Recommended(apiUrl, options);
-  console.log("Recommended");
-  console.table(res);
+//   res = await Recommended(apiUrl, options);
+//   console.log("Recommended");
+//   console.table(res);
 
-    const data = RequestData({
-      query: "Prey",
-      email: "kmoko@gmail.com",
-      mediaHandle: "@twiter/kokij",
-    });
-    res = await Request(apiUrl, data);
-    console.table(res);
-})();
+//     const data = RequestData({
+//       query: "Prey",
+//       email: "kmoko@gmail.com",
+//       mediaHandle: "@twiter/kokij",
+//     });
+//     res = await Request(apiUrl, data);
+//     console.table(res);
+// })();
+
+var a = {
+  _id: { $oid: "64ddc627f0e9bffac417818f" },
+  poster:
+    "https://xn--mycima-gsi.wecima.watch/wp-content/uploads/2023/06/Transformers-Rise-of-the-Beasts-2023.jpg",
+  src: "https://cima33.upbaam.com/6uvxg8efg8da/Transformers.Rise.of.the.Beasts.2023.1080p.WEB-DL.Wecima.tube.mp4.html?Key=JbngcHAbMOLqgWyie01_zw&Expires=1692012036",
+  title: "مشاهدة فيلم Transformers: Rise of the Beasts 2023 مترجم",
+};
--- frontend/src/utils/encryption/diphiri.js
+++ frontend/src/utils/encryption/diphiri.js
@@ -2,15 +2,15 @@ export const secret = {
   /**
    * @description secret to be used to generate key.
    */
-  k: "b59378bc-f136-4635-9361-7209086c1429",
+  k: "e26e71d7-ea50-478d-bd6b-b5795d06b33c",
   /**
    * @description secrte to be used to generate intialization vector
    */
-  iv: "6cd82594-799d-45d8-b471-5e83e35b23bf",
+  iv: "e5b2acb0-c6df-476d-9277-4dedcd8783d4",
   /**
    * @description Choose a fixed salt value or generate one based on your needs
    */
-  salt: "7837d11e-7377-4e37-8e04-2bdaa536955a",
+  salt: "17d9aaed-a12b-4173-99c2-68ff2ae126c4",
   /**
    * @description Choose an appropriate number of iterations
    */
