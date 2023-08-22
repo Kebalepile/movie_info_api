c29c0122eaf60c0dbb30c0b65759f609db04cc42
Kebalepile <pimpabutterfly86@gmail.com>
2023-08-22 14:37:43 +0200

    posters loaded dynamically

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -1,11 +1,15 @@
 import { initElementComponent } from "../../utils/nana.js";
-
-function Home() {
+import { Trending, Recommended } from "../cimaTube/api.js";
+import { apiUrl, options } from "../cimaTube/url.js";
+async function Home() {
+  /***
+   * @description add banner in home page
+   */
   document.body.appendChild(
     initElementComponent({
       type: "section",
       id: "home-page",
-      
+
       children: [
         {
           type: "article",
@@ -25,6 +29,94 @@ function Home() {
       ],
     })
   );
+  try {
+    const homePage = document.querySelector("#home-page");
+    const streamingNow = await Trending(apiUrl, options);
+    if (streamingNow.length) {
+      const trendingSlide = document.createElement("section");
+      trendingSlide.classList.add("movies_slide");
+      trendingSlide.setAttribute("id", "trending");
+      const h1 = document.createElement("h1");
+      h1.textContent = "Streaming Now";
+      const br = document.createElement("br");
+
+      const postersElem = document.createElement("div");
+      postersElem.classList.add("posters");
+
+      streamingNow.forEach((data) => {
+        const poster = document.createElement("figure");
+        poster.classList.add("poster");
+
+        const posterShadow = document.createElement("div");
+        posterShadow.classList.add("poster_shadow");
+
+        const playButton = document.createElement("span");
+        playButton.classList.add("play_button");
+        playButton.textContent = "▶";
+        posterShadow.appendChild(playButton);
+
+        const img = document.createElement("img");
+        img.src = data.poster;
+        img.alt = "Movie poster";
+        const caption = document.createElement("figcaption");
+        caption.textContent = data.title;
+
+        poster.appendChild(posterShadow);
+        poster.appendChild(img);
+        poster.appendChild(caption);
+        postersElem.appendChild(poster);
+      });
+      trendingSlide.appendChild(br);
+      trendingSlide.appendChild(h1);
+      trendingSlide.appendChild(br);
+      trendingSlide.appendChild(postersElem);
+      homePage.appendChild(trendingSlide);
+    }
+    const recommended = await Recommended(apiUrl, options);
+    if (recommended.length) {
+      const recommendedSlide = document.createElement("section");
+      recommendedSlide.classList.add("movies_slide");
+      recommendedSlide.setAttribute("id", "recommended");
+      const h1 = document.createElement("h1");
+      h1.textContent = "Recommended";
+      const br = document.createElement("br");
+
+      const postersElem = document.createElement("div");
+      postersElem.classList.add("posters");
+
+      recommended.forEach((data) => {
+        const poster = document.createElement("figure");
+        poster.classList.add("poster");
+
+        const posterShadow = document.createElement("div");
+        posterShadow.classList.add("poster_shadow");
+
+        const playButton = document.createElement("span");
+        playButton.classList.add("play_button");
+        playButton.textContent = "▶";
+        posterShadow.appendChild(playButton);
+
+        const img = document.createElement("img");
+        img.src = data.poster;
+        img.alt = "Movie poster";
+        img.setAttribute("loading", "lazy");
+        const caption = document.createElement("figcaption");
+        caption.textContent = data.title;
+
+        poster.appendChild(posterShadow);
+        poster.appendChild(img);
+        poster.appendChild(caption);
+        postersElem.appendChild(poster);
+      });
+      recommendedSlide.appendChild(br);
+      recommendedSlide.appendChild(h1);
+      recommendedSlide.appendChild(br);
+      recommendedSlide.appendChild(postersElem);
+      homePage.appendChild(recommendedSlide);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 Home();
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
 import nav from "./components/navigation/nav.js";
 nav();
 
-import {
-  Home,
-  Request,
-  Recommended,
-  Trending,
-} from "./components/cimaTube/api.js";
+// import {
+//   Home,
+//   Request,
+//   Recommended,
+//   Trending,
+// } from "./components/cimaTube/api.js";
 // import { apiUrl, options } from "./components/cimaTube/url.js";
 // import { Decrypt } from "./utils/encryption/encrypt.js";
 // import { RequestData } from "./components/cimaTube/Request.js";
@@ -31,11 +31,3 @@ import {
 //     res = await Request(apiUrl, data);
 //     console.table(res);
 // })();
-
-var a = {
-  _id: { $oid: "64ddc627f0e9bffac417818f" },
-  poster:
-    "https://xn--mycima-gsi.wecima.watch/wp-content/uploads/2023/06/Transformers-Rise-of-the-Beasts-2023.jpg",
-  src: "https://cima33.upbaam.com/6uvxg8efg8da/Transformers.Rise.of.the.Beasts.2023.1080p.WEB-DL.Wecima.tube.mp4.html?Key=JbngcHAbMOLqgWyie01_zw&Expires=1692012036",
-  title: "مشاهدة فيلم Transformers: Rise of the Beasts 2023 مترجم",
-};
