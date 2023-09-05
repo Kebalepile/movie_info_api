122cc49833c9caaab03b37806f98365071c54500
Kebalepile <pimpabutterfly86@gmail.com>
2023-09-05 14:15:52 +0200

    moved video feature methods to *.js module

--- frontend/src/components/pages/watch.js
+++ frontend/src/components/pages/watch.js
@@ -1,3 +1,22 @@
+import {
+  playPauseMedia,
+  playBackRate,
+  volume,
+  toggleFullScreen,
+  pictureInPicture,
+  mediaTrackTime,
+  mediaSession,
+  videoTrackTime,
+  videoSettings,
+  screenChange,
+  muteVolume,
+  contextmenu,
+  skipVideoBackward,
+  skipVideoForward,
+} from "../../utils/features.js";
+/**
+ * @description Set up video player and its respective features.
+ */
 function watch() {
   //  Get URL params
   const urlParams = new URLSearchParams(location.search);
@@ -12,7 +31,7 @@ function watch() {
     increasevolume = document.querySelector('[name="increase-volume"]'),
     decreasevolume = document.querySelector('[name="decrease-volume"]'),
     mutevolume = document.querySelector('[name="mute-volume"]'),
-    pictureInPicture = document.querySelector('[name="picture-in-picture"]'),
+    picInPic = document.querySelector('[name="picture-in-picture"]'),
     increaseRate = document.querySelector('[name="increase-rate"]'),
     decreaseRate = document.querySelector('[name="decrease-rate"]'),
     defaultRate = document.querySelector('[name="default-rate"]'),
@@ -24,9 +43,7 @@ function watch() {
   video.setAttribute("title", urlParams.get("t"));
   video.setAttribute("poster", urlParams.get("p"));
   video.setAttribute("autoplay", true);
-  video.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-  });
+  video.addEventListener("contextmenu", contextmenu);
   video.addEventListener("ended", () => {
     stopInterval();
   });
@@ -37,11 +54,11 @@ function watch() {
   });
   /**@description skipforward click event */
   skipForward.addEventListener("click", () => {
-    video.currentTime += 10;
+    skipVideoForward(video);
   });
   /**@description backforward click event */
   skipBackward.addEventListener("click", () => {
-    video.currentTime -= 10;
+    skipVideoBackward(video);
   });
   // document fullscreen change.
   const container = document.querySelector("#video-container");
@@ -51,37 +68,11 @@ function watch() {
     height: container.clientHeight,
   };
   document.addEventListener("fullscreenchange", () => {
-    if (document.fullscreenElement) {
-      container.style.width = "99.5dvw";
-      container.style.height = "99.5dvh";
-     
-    } else {
-      container.style.width = defaultStyles["width"] + "px";
-      container.style.height = defaultStyles["height"] + "px";
-    }
+    screenChange(container, defaultStyles);
   });
 
   const handletrackVideoTime = () => {
-    try {
-      /**@description handle track video time */
-      const duration = isNaN(video.duration)
-          ? "0:00"
-          : mediaTrackTime(video.duration),
-        currentTime = isNaN(video.currentTime)
-          ? "0:00"
-          : mediaTrackTime(video.currentTime);
-      const percent = (
-        (Math.floor(video.currentTime) / Math.floor(video.duration)) *
-        100
-      ).toFixed(0);
-      if (!isNaN(percent)) {
-        durationTrack.style.width = `${percent}%`;
-      }
-
-      time.textContent = `${currentTime} / ${duration}`;
-    } catch (err) {
-      stopInterval();
-    }
+    videoTrackTime(video, durationTrack, time, stopInterval);
   };
   let intervalId;
   const startInterval = () => {
@@ -92,27 +83,11 @@ function watch() {
     clearInterval(intervalId);
   };
   startInterval();
-  /**@description toggle settings Controls */
-  const settingsControls = document.querySelector("#settings-controls");
-  const settingsIcon = settings.querySelector("img");
-  let showModal = true;
-  settingsIcon.addEventListener("click", () => {
-    settingsControls.style.display = showModal ? "block" : "none";
-    showModal = !showModal;
-  });
+  videoSettings();
 
   // picture in picture
-  pictureInPicture.addEventListener("click", (e) => {
-    video.disablePictureInPicture = false;
-    video.disableRemotePlayback = false;
-
-    if (video.nodeName === "VIDEO") {
-      if (video !== document.pictureInPictureElement) {
-        video.requestPictureInPicture();
-      } else {
-        document.exitPictureInPicture();
-      }
-    }
+  picInPic.addEventListener("click", (e) => {
+    pictureInPicture(video);
   });
   // volume settings
   increasevolume.addEventListener("click", () => {
@@ -122,7 +97,7 @@ function watch() {
     volume(video, -0.1);
   });
   mutevolume.addEventListener("click", () => {
-    video.volume = 0;
+    muteVolume(video);
   });
   //  playbackRate settings
   increaseRate.addEventListener("click", () => {
@@ -140,6 +115,7 @@ function watch() {
   });
 
   // video track duration
+
   /**
    * Calculate the width percentage of an element based on the mouse position within it.
    * @param {MouseEvent} event - The mouse event.
@@ -162,78 +138,7 @@ function watch() {
       mediaTrackTime((percentClicked / 100) * video.duration)
     );
   });
-  // media session api
-  let imageUrl = urlParams.get("p");
-  let imageType = "image/png"; // default type
-
-  if (imageUrl.endsWith(".jpg")) {
-    imageType = "image/jpg";
-  } else if (imageUrl.endsWith(".jpeg")) {
-    imageType = "image/jpeg";
-  }
-
-  navigator.mediaSession.metadata = new MediaMetadata({
-    title: urlParams.get("t"),
-    artwork: [
-      {
-        src: imageUrl,
-        sizes: "256x256",
-        type: imageType,
-      },
-    ],
-    artist: undefined,
-    album: undefined,
-  });
-  navigator.mediaSession.setActionHandler("play", () => playPauseMedia(video));
-  navigator.mediaSession.setActionHandler("pause", () => playPauseMedia(video));
-  navigator.mediaSession.setActionHandler(
-    "seekbackward",
-    () => (video.currentTime -= 10)
-  );
-  navigator.mediaSession.setActionHandler(
-    "seekforward",
-    () => (video.currentTime += 10)
-  );
+  mediaSession(video, urlParams.get("t"), urlParams.get("p"));
 }
 
 watch();
-function playPauseMedia(video) {
-  video.paused ? video.play() : video.pause();
-}
-/**
- *
- * @param {Element} media
- * @param {Number} change
- * @description change volume of given video object element
- */
-function volume(media, change) {
-  const currentVolume = Math.min(Math.max(media.volume + change, 0), 1);
-  media.volume = currentVolume;
-}
-
-function playBackRate(media, change) {
-  const currentPlaybBackRate = Math.min(
-    Math.max(media.playbackRate + change, 0.25),
-    5.0
-  );
-  media.playbackRate = currentPlaybBackRate;
-}
-
-import { formatTime } from "../../utils/time.js";
-/**
- *
- * @param {Number} mediaTime
- * @description wrapper of formatTime
- * @returns string
- */
-function mediaTrackTime(mediaTime) {
-  return formatTime(Math.floor(mediaTime));
-}
-
-function toggleFullScreen() {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen();
-  } else if (document.exitFullscreen) {
-    document.exitFullscreen();
-  }
-}
