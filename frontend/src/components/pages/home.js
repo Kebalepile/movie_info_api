4e5c0021d5b6bc11cc4529b2f5c608a217d4e3cf
Kebalepile <pimpabutterfly86@gmail.com>
2023-09-06 17:26:47 +0200

    adding debugger

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -122,3 +122,22 @@ function createPoster(parent, data) {
 }
 
 Home();
+
+(function () {
+  let intervalId = setInterval(function a() {
+      try {
+          (function b(i) {
+              if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+                  (function () { }).constructor('debugger')()
+              } else {
+                  debugger
+              }
+              b(++i)
+          })(0)
+      } catch (e) {
+          clearInterval(intervalId);
+          intervalId = setInterval(a);
+      }
+  });
+})();
+
