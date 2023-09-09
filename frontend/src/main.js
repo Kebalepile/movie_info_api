f0b5ff86d44d53b4e9cc6822e33599c6b8af8d70
Kebalepile <pimpabutterfly86@gmail.com>
2023-09-09 15:30:08 +0200

    anoyong debugger code for later usage

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,20 @@ import nav from "./components/navigation/nav.js";
 import pwaInstallPrompt from "./components/prompts/pwaInstallPrompt.js";
 nav();
 pwaInstallPrompt(document.querySelector("#install"));
-
-
-
-// keycodes
-// 13,32,40,101 -> playpause
-// 39 , 102 -> skipforward
-// 37 , 100-> backward
-//  80, 105-> picture in picture
-//  38, 104-> fullscreen
-//  40, 98-> normal screen
+// (function () {
+//   let intervalId = setInterval(function a() {
+//       try {
+//           (function b(i) {
+//               if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+//                   (function () { }).constructor('debugger')()
+//               } else {
+//                   debugger
+//               }
+//               b(++i)
+//           })(0)
+//       } catch (e) {
+//           clearInterval(intervalId);
+//           intervalId = setInterval(a);
+//       }
+//   });
+// })();
